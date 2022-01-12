4ef8a9d4caf815de0a6cd1d2aeaea73a2b7dc939
nadiaf85 <nadia.fiorio@alice.it>
2022-01-12 18:45:38 +0100

    modifiche varie

--- js/main.js
+++ js/main.js
@@ -1,20 +1,9 @@
-// Milestone 1
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per
-// ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
-// Milestone 2
-// Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà
-// per visualizzare le icone del colore corrispondente.
+
+
 // Milestone 3
 // Aggiungere alla pagina una select in cui le options corrispondono
 // ai vari tipi di icone (animal, vegetable, user). Quando l’utente 
 //seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-// BONUS
-// 1- modificare la struttura dati fornita e valorizzare la proprietà
-// “color” in modo dinamico: generare in modo casuale un codice colore,
-// sapendo che la notazione esadecimale è formata dal simbolo “#” seguito
-// da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
-// 2- popolare le options della select della milestone 3 dinamicamente.
-
 
 
 const icons = [
@@ -137,17 +126,52 @@ const icons = [
 let container = document.getElementById("icons_container");
 
 
-// Creo ciclo per inserire i box
+// Creo funzione per inserire i box
 
-for( let i=0; i<icons.length;i++){
+function creaBox() {
+    for( let i=0; i<icons.length;i++)
 
-	container.innerHTML+= `
-    <div class="col-2 d-flex align-items-center justify-content-center box">
-	    <div class="${icons[i].color}">
-            <i class=“${icons[i].family} ${icons[i].prefix}-${icons[i].type}“></i>
-		    <div class="nome">${icons[i].name}</div>
-	    </div> 
-    </div>
+	    container.innerHTML+= `
+            <div class="col-2 d-flex align-items-center justify-content-center text-center box">
+	            <div class="${icons[i].color}">
+                <i class="${icons[i].family} ${icons[i].prefix}${icons[i].name}"></i>
+		        <div class="nome">${icons[i].name}</div>
+	            </div> 
+            </div>
 		
-		`
-};
+		    `
+  };
+
+  creaBox(icons);
+
+
+// let tipologia = document.getElementById('tipologia');
+
+// function sceltaTipo() {
+//     tipologia.addEventListener('change');{
+
+//         if(tipologia == "all"){
+//             container.innerHTML += "";
+            
+//         } if(tipologia == "animal"){
+//             container.innerHTML += "";
+            
+//         } if (tipologia == "vegetable"){
+//             container.innerHTML += "";
+            
+//         } if (tipologia == "user"){
+//             container.innerHTML += "";
+            
+//         }
+//     }
+// };
+
+let tipologia = document.getElementById('tipologia');
+
+tipologia.addEventListener('change',function () {
+
+    if(tipologia == "all"){
+        container.innerHTML = "";
+    }
+   
+});
