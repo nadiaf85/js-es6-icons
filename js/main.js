62a58172436878e1b0a2a5e41eab5fec5020ff12
nadiaf85 <nadia.fiorio@alice.it>
2022-01-12 16:26:11 +0100

    inseriti box

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-const icon = [
+// Milestone 1
+// Partendo dalla struttura dati fornita, visualizzare in pagina un box per
+// ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
+// Milestone 2
+// Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà
+// per visualizzare le icone del colore corrispondente.
+// Milestone 3
+// Aggiungere alla pagina una select in cui le options corrispondono
+// ai vari tipi di icone (animal, vegetable, user). Quando l’utente 
+//seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+// BONUS
+// 1- modificare la struttura dati fornita e valorizzare la proprietà
+// “color” in modo dinamico: generare in modo casuale un codice colore,
+// sapendo che la notazione esadecimale è formata dal simbolo “#” seguito
+// da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+// 2- popolare le options della select della milestone 3 dinamicamente.
+
+
+
+const icons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +131,23 @@ const icon = [
 		color: 'blue'
 	}
 ];
+
+
+
+let container = document.getElementById("icons_container");
+
+
+// Creo ciclo per inserire i box
+
+for( let i=0; i<icons.length;i++){
+
+	container.innerHTML+= `
+    <div class="col-2 d-flex align-items-center justify-content-center box">
+	    <div class="${icons[i].color}">
+		    <i class=" ${icons[i].family}-${icons[i].prefix}${icons[i].type}"></i>
+		    <div class="nome">${icons[i].name}</div>
+	    </div> 
+    </div>
+		
+		`
+};
